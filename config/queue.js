eded387efbe6c90a940feb6bf0a41b114af998a3
Rikky <son.levuthai@gmail.com>
2017-04-12 03:20:11 +0700

    integrate pm2

--- config/queue.js
+++ config/queue.js
@@ -1,6 +1,18 @@
 module.exports = {
-    worker: {
-        retry: 1,
-        timeout: 10000
+
+    'default': process.env.QUEUE_DEFAULT || 'general',
+
+    'adapters': {
+        'send-email': {
+            'adapter': 'synced'
+        },
+        'general': {
+            'adapter': 'dumb',
+            'worker': {
+                number  : 1,
+                retry   : 1,
+                timeout : 10000
+            },
+        }
     }
 };
